91f1112592d44f8b8f6792fbba602a4e2858356d
anand1996wani <anand.wani1996@gmail.com>
2018-09-05 18:00:16 +0530

    sigsuspend implemainted successfully

--- lab2/signal.c
+++ lab2/signal.c
@@ -18,16 +18,17 @@ int main ()
   signal (SIGINT, int_handler);
   signal (SIGTSTP, stop_handler);
   signal (SIGQUIT, quit_handler);
-  sigset_t originalMask,blockMask,waitingMask;
+  sigset_t originalMask,blockMask,waitingMask, suspend;
   sigaddset(&blockMask,SIGINT);
   sigaddset(&blockMask,SIGQUIT);
   sigaddset(&blockMask,SIGTSTP);
+  sigaddset(&suspend,SIGQUIT);
   sigprocmask(SIG_BLOCK,&blockMask,&originalMask);
   printf ("Entering infinite loop\n");
   int i = 0;
   while (i==0)
     {
-      sleep (30);
+      sleep (10);
       i++;
     }
     sigpending(&waitingMask);
@@ -40,6 +41,11 @@ int main ()
     if(sigismember(&waitingMask,SIGQUIT)){
     	printf("Pending Signal Is SigQuit\n");
     }
+    
+  //sigsuspend(&suspend);
+  /*
+  	Sigsuspend waits for any number of instances of signal specified in suspend and unblocks when any other signal is received.. 
+  */
   printf (" This is unreachable\n");
   return 0;
 }
