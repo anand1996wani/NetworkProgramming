d534ae92cd767627e7cfdaa4ea1428d3fd7b19d4
anand1996wani <anand.wani1996@gmail.com>
2018-09-05 17:46:14 +0530

    Lab 2 Completed

--- lab2/signal.c
+++ lab2/signal.c
@@ -1,5 +1,6 @@
 #include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 void stop_handler(int signo)
 {
@@ -12,17 +13,35 @@ void quit_handler(int signo)
 }
 
 void int_handler(int signo);
-main ()
+int main ()
 {
   signal (SIGINT, int_handler);
   signal (SIGTSTP, stop_handler);
   signal (SIGQUIT, quit_handler);
+  sigset_t originalMask,blockMask,waitingMask;
+  sigaddset(&blockMask,SIGINT);
+  sigaddset(&blockMask,SIGQUIT);
+  sigaddset(&blockMask,SIGTSTP);
+  sigprocmask(SIG_BLOCK,&blockMask,&originalMask);
   printf ("Entering infinite loop\n");
-  while (1)
+  int i = 0;
+  while (i==0)
     {
-      sleep (10);
+      sleep (30);
+      i++;
+    }
+    sigpending(&waitingMask);
+    if(sigismember(&waitingMask,SIGINT)){
+    	printf("Pending Signal Is Ctrl-C\n");
+    }
+    if(sigismember(&waitingMask,SIGTSTP)){
+    	printf("Pending Signal Is Ctrl-Z\n");
+    }
+    if(sigismember(&waitingMask,SIGQUIT)){
+    	printf("Pending Signal Is SigQuit\n");
     }
   printf (" This is unreachable\n");
+  return 0;
 }
 
  /* will be called asynchronously, even during a sleep */
--- lab2/sigsegv.c
+++ lab2/sigsegv.c
@@ -8,6 +8,10 @@ void sigsegvHandler(){
 int main(){
 	int n;
 	signal(SIGSEGV,sigsegvHandler);
+	sigset_t origMask,blockMask;
+	sigemptyset(&blockMask);
+	sigaddset(&blockMask,SIGSEGV);
+	sigprocmask(SIG_BLOCK,&blockMask,NULL);//Not Possible To block
 	printf("Enter the number\n");
 	scanf("%d",n);
 	printf("Continue After SEGSEGV\n");
